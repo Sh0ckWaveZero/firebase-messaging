47b04d55969d8792af0559e5a4c33989744b2a76
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-06-08 11:10:50 +0700

    chore: Add Firebase Admin SDK and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,75 @@
+import { env } from 'bun';
+import * as serviceAccount from "../config/serviceAccountKey.json";
 import { Elysia } from "elysia";
+import admin from "firebase-admin";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const initializeFirebase = () => {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    });
+    console.log("🔥 Firebase initialized successfully.");
+  } catch (error) {
+    console.error("❌ Failed to initialize Firebase: ", error);
+  }
+}
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const createMessage = (): admin.messaging.Message => {
+  // TODO: Replace with your actual FCM token
+  const fvmToken = env.FCM_TOKEN ?? '';
+
+  return {
+    token: fvmToken,
+    notification: {
+      "title": "NEW OPD",
+      "body": "นายมานะ มานะมา",
+    },
+    android: {
+      notification: {
+        "title": "NEW OPD",
+        "body": "นายมานะ มานะมา",
+      },
+    },
+    data: {
+      id: '1',
+      title: 'Notification Title',
+      type: 'Notification Type',
+      name: 'Notification Name',
+      time: 'Notification Time',
+      icon: 'Notification Icon',
+      isRead: 'false',
+    }
+  };
+}
+
+const sendMessage = async (message: admin.messaging.Message) => {
+  try {
+    const response = await admin.messaging().send(message);
+    console.log('Successfully sent message:', response);
+    return {
+      status: 200,
+      message: 'Successfully sent message'
+    }
+  } catch (error) {
+    console.log('Error sending message:', error);
+  }
+};
+
+const handlePushMessage = async () => {
+  const message = createMessage();
+  return sendMessage(message);
+};
+
+const startServer = () => {
+  const app = new Elysia()
+    .get("/", () => "Hello Elysia")
+    .post("/pushMessage", handlePushMessage)
+    .listen(3000);
+
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+}
+
+initializeFirebase();
+startServer();
